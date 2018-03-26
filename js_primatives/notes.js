19049feecff608338a8abb4570e914be13d41a59
judeharding <jharding@genesisgroupusa.com>
2018-03-26 14:57:19 -0400

    changed file names

--- js_primatives/notes.js
+++ js_primatives/notes.js
@@ -12,11 +12,18 @@ setInterval(function(){
 	numbers - whole, fractional, negative,  modulus(remainder)
 	strings - "text" or 'text' or "hi " + "jude"
 	booleans - true/false
-	null - values
-	undefined - values
+	null - value is nothing (color // has yet to be defined)
+	undefined - value is nothing (var age; // no value YET)
 
 variables are containers which data is stored (primative datatypes) to recall later
 variable names:
     camelCase
     snake_case
     kebob-case
+
+METHODS
+    alert(); // brings up a modal
+    prompt(); // gets data from a user
+    console.log(); // prints to the console only that can be stored in a var.  var name = prompt ("enter name")
+    comments
+    clear() // methods have() after them that come with js
---
+++ js_primatives/script.js
@@ -0,0 +1,5 @@
+// alert("hello from the js file");
+
+var userName = prompt("what is your name?");
+alert("nice to see you " + userName);
+console.log("see you later " + userName);
