294e0c896a0d80461f2474600162473170538d2d
judeharding <jharding@genesisgroupusa.com>
2018-03-26 17:01:54 -0400

    updating my answers even though i am correct too

--- conditionals/script.js
+++ conditionals/script.js
@@ -1,19 +1,29 @@
 console.log("consript working");
 
-var age = prompt("enter in your age ");
+// var age = prompt("enter in your age "); my answer
 
+var age = Number(prompt("what is your age?")); //get the age and convert to a number
+
+
+// if age is negative
 if (age <=0) {
     console.log("ERROR - you aren't alive yet");
 }
 
+// if age is 21
 if (age === 21) {
     console.log("happy 21st bd");
 }
 
-if (age % 2) {
+
+// if age is odd
+// if (age % 2) {    my answer
+if (age % 2 !==0) {
     console.log("your age is odd");
 }
 
-if (Math.sqrt(age)) {
+// if age is a perfect square
+// if (Math.sqrt(age)) {  my answer
+if (age % Math.sqrt(age)===0) {
     console.log("perfect square");
 }
