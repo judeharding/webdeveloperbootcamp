68b5ab9cf42aa36086d264660277da06d2f6ad4d
judeharding <jharding@genesisgroupusa.com>
2018-03-26 14:37:56 -0400

    notes for primatives

---
+++ primatives/index.js
@@ -0,0 +1,22 @@
+NOTES for JS primatives
+
+cool google logo expand
+
+var logo = document.querySelector("#hplogo");
+setInterval(function(){
+	logo.width+=5;
+}, 100)
+
+
+5 primitive datatypes
+	numbers - whole, fractional, negative,  modulus(remainder)
+	strings - "text" or 'text' or "hi " + "jude"
+	booleans - true/false
+	null - values
+	undefined - values
+
+variables are containers which data is stored (primative datatypes) to recall later
+variable names:
+    camelCase
+    snake_case
+    kebob-case
