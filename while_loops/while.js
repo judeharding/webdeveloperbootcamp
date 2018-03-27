94992486458b4e1119e3170191033ab6bfee9d4a
judeharding <jharding@genesisgroupusa.com>
2018-03-27 14:32:54 -0400

    while loops fixed my infinite loop

--- while_loops/while.js
+++ while_loops/while.js
@@ -9,7 +9,6 @@ while (counter < 20) {
 }
 
 // print all even numbers between 10 and 40
-
 var evens = 10;
 
 while (evens < 41) {
@@ -19,10 +18,25 @@ while (evens < 41) {
 }
 
 // print all odd numbers between 300 and 333
+var odds = 300;
 
-// var odds = 300;
-//
-// while ((odds % 2 !==0 ) < 334) {
-//     console.log("odds are " + odds);
-//     odds ++;
-// }
+while (odds < 334) {
+    if (odds % 2 !==0) {
+        console.log("odds are " + odds);
+    }
+    odds ++;
+}
+
+// print all numbers divisible by 5 and 3 between 5 and 50
+
+var x = 5
+
+while (x < 50) {
+    if (x % 5 == 0) {
+        console.log("x is divisible by 5 " + x);
+    } else if (x % 3 == 0) {
+        console.log(" x is divisible by 3 " + x);
+    }
+
+    x = x + 1;
+}
