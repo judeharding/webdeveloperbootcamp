a5a7ff871c0127660e44e5da511957497959d151
judeharding <jharding@genesisgroupusa.com>
2018-03-29 14:45:28 -0400

    todo j

--- jtodo/jtodo.js
+++ jtodo/jtodo.js
@@ -1,1 +1,22 @@
 // console.log("msg from jtodo");
+
+var todos = [];
+
+var input = prompt("What would you like to do?  New, List or Quit");
+
+while (input !== "quit") {
+    // handle input
+    if (input === "list") {
+        console.log(todos);
+    } else if (input === "new") {
+        // ask for new todo
+        var newTodo = prompt("Enter new Todo");
+        // add new to todos List
+        todos.push(newTodo);
+    }
+
+    // ask again for input
+    input = prompt("What would you like to do?  New, List or Quit");
+
+}
+console.log("Ok, you quit the app");
