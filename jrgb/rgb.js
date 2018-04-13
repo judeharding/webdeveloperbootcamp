ac840c3b45e5d86536f536e1a19bc70ebc65d78e
judeharding <jharding@genesisgroupusa.com>
2018-04-12 22:42:17 -0400

    game fixed - back on track

--- jrgb/rgb.js
+++ jrgb/rgb.js
@@ -10,16 +10,29 @@
 // ]
 
 var colors = generateRandomColors(6);
-var pickedColor;
+// var pickedColor;
 var squares = document.querySelectorAll(".square");
 var pickedColor = pickColor();
 var colorDisplay = document.getElementById("colorDisplay");
 var messageDisplay = document.querySelector("#message");
 var h1 = document.querySelector("h1");
+var resetButton = document.querySelector("#reset");
 
 
+resetButton.addEventListener("click", function(){
+    // generate all new colors
+    colors = generateRandomColors(6);
+    // pick new randome color
+    pickedColor = pickColor();
+    // change color display to match new picked colors
+    colorDisplay.textContent = pickedColor;
+    // change colors of squares
+    for (var i = 0; i < squares.length; i++) {
+        squares[i].style.backgroundColor = colors[i];
+    }
+    h1.style.backgroundColor = "#232323";
 
-
+})
 
 colorDisplay.textContent = pickedColor;
 
@@ -27,7 +40,7 @@ for (var i = 0; i < squares.length; i++) {
     // add initial color to squares
     squares[i].style.backgroundColor = colors[i];
 
-    // add click listeners to squares
+    // add click listeners to squares to figure out color clicked
     squares[i].addEventListener("click", function(){
         // grab color of clicked squares
         var clickedColor = this.style.backgroundColor;
@@ -35,10 +48,11 @@ for (var i = 0; i < squares.length; i++) {
         //compare color to pickedColor
         if (clickedColor === pickedColor) {
             messageDisplay.textContent = "CORRECT!!!";
+            resetButton.textContent = "Play Again???"
             changeColors(clickedColor);
             h1.style.backgroundColor = clickedColor;
         } else {
-            this.style.backgroundColor = "#232323";
+            this.style.backgroundColor = "#232323"; // makes wrong color disappear
             messageDisplay.textContent = "Try Again";
         }
     });
@@ -55,7 +69,7 @@ function changeColors(color){
 function pickColor(){
     // Math.floor(Math.random() * 6 + 1);
     var random = Math.floor(Math.random() * colors.length);
-    return colors[random];
+    return colors[random]; // to access one of the squares
 }
 
 function generateRandomColors(num){
@@ -67,20 +81,20 @@ function generateRandomColors(num){
         //get random color and push into array
         arr.push(randomColor());
     }
+
     //return
     return arr;
 }
 
 function randomColor(){
     // pick a red from 0 - 255
-    var red = Math.floor (Math.random() * 256);
-
+    var r = Math.floor (Math.random() * 256);
     // pick a red from 0 - 255
-    var green = Math.floor (Math.random() * 256);
-
+    var g = Math.floor (Math.random() * 256);
     // pick a red from 0 - 255
-    var blue = Math.floor (Math.random() * 256);
-    //formulate the rgb
-    return "rbg(" + red + ", " + green + ", " + blue + ")";
+    var b = Math.floor (Math.random() * 256);
+    //formulate the "rgb(r, g, b)"
+
+    return "rgb(" + r + ", " + g + ", " + b + ")";
 
 }
